6415806ccd103888aeb7fdb102908fda0ffc2804
exleyt <81730686+exleyt@users.noreply.github.com>
2022-06-19 04:39:08 -0700

    Added command deploy handling through file

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,8 +1,29 @@
+// Require the necessary node.js classes
+const fs = require('node:fs');
+const path = require('node:path');
+
+// Require the necessary discord.js classes
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+
+// Require the necessary authentification tokens
 const { clientId, guildId, token } = require('./auth.json');
 
 const commands = [];
+
+// Defines path to command files
+const commandsPath = path.join(__dirname, 'commands');
+
+// Filters files from commandPath for the .js extension
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// Populates commands for each of the commandFiles
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Push a new item to the commands list
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(token);
 
