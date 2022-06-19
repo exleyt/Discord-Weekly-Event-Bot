13f3bcf8c753227deb76233e21b90b018c6a7017
exleyt <81730686+exleyt@users.noreply.github.com>
2022-06-18 21:51:26 -0700

    Added command handling through files

--- main.js
+++ main.js
@@ -1,14 +1,48 @@
-// https://discordjs.guide/creating-your-bot/
+// Require the necessary node.js classes
+const fs = require('node:fs');
+const path = require('node:path');
+
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./auth.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+
+// Defines path to command files
+const commandsPath = path.join(__dirname, 'commands');
+
+// Filters files from commandPath for the .js extension
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// Populates client.commands for each of the commandFiles
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try { await command.execute(interaction); }
+	catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 // Login to Discord with your client's token
