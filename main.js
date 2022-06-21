94e4cd354ea2f43fb42029a9afdea5b62252e9e4
exleyt <81730686+exleyt@users.noreply.github.com>
2022-06-21 02:01:02 -0700

    Still errors even if a reply has already been sent

--- main.js
+++ main.js
@@ -41,7 +41,8 @@ client.on('interactionCreate', async interaction => {
 	try { await command.execute(interaction); }
 	catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		try { await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }); }
+		catch { await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true }); }
 	}
 });
 
