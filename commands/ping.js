74dda4a0803883b9309bc635450d1992d5ee2a54
exleyt <81730686+exleyt@users.noreply.github.com>
2022-06-20 20:16:37 -0700

    Reordered parameters

--- commands/ping.js
+++ commands/ping.js
@@ -5,45 +5,45 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!')
-		.addStringOption(option =>
-			option.setName('name')
-				.setDescription('the name of the event to post for')
-				.setRequired(true))
-		.addMentionableOption(option =>
+		.addRoleOption(option =>
 			option.setName('mentionable')
 				.setDescription('mention something')
 				.setRequired(true))
 		.addStringOption(option =>
-			option.setName('days')
-				.setDescription('which days of the week to repeat for \'mtwhfsu\'')
+			option.setName('name')
+				.setDescription('the name of the event to post for')
 				.setRequired(true))
 		.addStringOption(option =>
 			option.setName('start_day')
 				.setDescription('what day of the year to start on \'yyyy-mm-dd\' local')
 				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('start_time')
+				.setDescription('what time of the day to start at \'hh:mm\' local')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('days')
+				.setDescription(`which days of the week to repeat for \'${daysOfWeek}\'`)
+				.setRequired(true))
 		.addIntegerOption(option =>
 			option.setName('size')
-				.setDescription('the number of time slots for each day')
+				.setDescription('the number of time slots for each day 1-10')
 				.setRequired(true))
 		.addIntegerOption(option =>
 			option.setName('length')
 				.setDescription('the number of minutes for each time slot')
-				.setRequired(true))
-		.addStringOption(option =>
-			option.setName('start_time')
-				.setDescription('what time of the day to start at \'hh:mm\' local')
 				.setRequired(true)),
 	async execute(interaction) {
+		const mentionable = interaction.options.getMentionable('mentionable');
 		const name = interaction.options.getString('name');
-		// const mentionable = interaction.options.getMentionable('mentionable');
-		// const days = interaction.options.getString('days');
 		const startDay = interaction.options.getString('start_day');
-		// const size = interaction.options.getInteger('size');
-		// const length = interaction.options.getInteger('length');
 		const startTime = interaction.options.getString('start_time');
 		const startDate = new Date(`${startDay}T${startTime}`);
 		const startEpoch = startDate.getTime() / 1000;
 		await interaction.reply(`This is the signup sheet for ${name} <t:${startEpoch}:D> which starts <t:${startEpoch}:R>. Please react accordingly.`);
+		const days = interaction.options.getString('days');
+		const size = interaction.options.getInteger('size');
+		const length = interaction.options.getInteger('length');
 		await interaction.followUp('Pingu!');
 	},
 };
