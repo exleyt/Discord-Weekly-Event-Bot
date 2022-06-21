1bab6c2d523a932c50c04d6ef2e6e2aa5a29ed16
exleyt <81730686+exleyt@users.noreply.github.com>
2022-06-20 21:11:03 -0700

    Fixed deleteUrl for groupless delete

--- deploy-commands.js
+++ deploy-commands.js
@@ -33,7 +33,7 @@ rest.get(Routes.applicationCommands(clientId))
 	.then(data => {
 		const promises = [];
 		for (const command of data) {
-			const deleteUrl = `${Routes.applicationGuildCommands(clientId)}/${command.id}`;
+			const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`;
 			promises.push(rest.delete(deleteUrl));
 		}
 		return Promise.all(promises);
